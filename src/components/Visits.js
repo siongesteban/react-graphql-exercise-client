039f0ba6a250ef8abfc50255a3375d353dd89c7f
Siong Esteban <siong.esteban@gmail.com>
2019-09-03 03:49:50 -0400

    Create initial "Visits" table

--- src/components/Visits.js
+++ src/components/Visits.js
@@ -1,26 +1,180 @@
 import React, { useEffect, useState } from 'react';
 import { gql } from 'apollo-boost';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
+import client from '../graphql-client';
+
+import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-import client from '../graphql-client';
-
-import Title from './Title';
-
-// Generate Order Data
-function createData(id, datetime, device, ip) {
-  return { id, datetime, device, ip };
-}
+const createData = (datetime, device, ip) => ({
+  datetime, device, ip
+});
 
 const rows = [
-  createData(0, '16 Mar, 2019', 'POCOPHONE F1', '192.168.254.254'),
+  createData('Date 123', 'POCOPHONE F1', '192.168.254.254.'),
+  createData('Date 321', 'IPHONE X', '192.168.254.254.'),
 ];
 
-export default () => {
+const desc = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const stableSort = (array, cmp) => {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+
+  return stabilizedThis.map(el => el[0]);
+}
+
+const getSorting = (order, orderBy) =>
+  order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+
+const headCells = [
+  { id: 'datetime', numeric: false, disablePadding: true, label: 'Datetime' },
+  { id: 'device', numeric: false, disablePadding: false, label: 'Device' },
+  { id: 'ip', numeric: false, disablePadding: false, label: 'IP Address' },
+];
+
+const EnhancedTableHead = props => {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={order}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles(theme => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+        color: theme.palette.secondary.main,
+        backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+      }
+      : {
+        color: theme.palette.text.primary,
+        backgroundColor: theme.palette.secondary.dark,
+      },
+  spacer: {
+    flex: '1 1 100%',
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+}));
+
+const EnhancedTableToolbar = props => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      <div className={classes.title}>
+        <Typography variant="h6" id="tableTitle">
+          Visits
+        </Typography>
+      </div>
+      <div className={classes.spacer} />
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  table: {
+    minWidth: 750,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('calories');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
   const [visits, setVisits] = useState([]);
 
   useEffect(() => {
@@ -29,43 +183,97 @@ export default () => {
         query: gql`
           {
             visits {
-              datetime,
-              device,
+              datetime
+              device
               ip
             }
           }
         `,
       });
 
-      console.log('response', response);
-
       setVisits(response.data.visits);
-    };
+    }
 
     getVisits();
-  }, [])
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isDesc = orderBy === property && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, visits.length - page * rowsPerPage);
 
   return (
-    <>
-      <Title>Visits</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Datetime</TableCell>
-            <TableCell>Device</TableCell>
-            <TableCell>IP Address</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {visits.map(visit => (
-            <TableRow key={visit.ip}>
-              <TableCell>{visit.datetime}</TableCell>
-              <TableCell>{visit.device}</TableCell>
-              <TableCell>{visit.ip}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
-  )
-};
+    <div className={classes.root}>
+      <EnhancedTableToolbar />
+      <div className={classes.tableWrapper}>
+        <Table
+          className={classes.table}
+          aria-labelledby="tableTitle"
+          size="medium"
+        >
+          <EnhancedTableHead
+            classes={classes}
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            rowCount={visits.length}
+          />
+          <TableBody>
+            {stableSort(visits, getSorting(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.ip}
+                  >
+                    <TableCell component="th" id={labelId} scope="row" padding="none">
+                      {row.datetime}
+                    </TableCell>
+                    <TableCell>{row.device}</TableCell>
+                    <TableCell>{row.ip}</TableCell>
+                  </TableRow>
+                );
+              })}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 49 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={visits.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'previous page',
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'next page',
+        }}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </div>
+  );
+}
